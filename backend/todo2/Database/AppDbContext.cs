1a0c41c64de3598c6db0b03a6def1312e225ec76
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-09 23:29:59 +0100

    Add AppDbContext for EF Core entity management

---
+++ backend/todo2/Database/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo2.Models.Db;
+
+namespace todo2.Database;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<TodoList> TodoLists => Set<TodoList>();
+    public DbSet<TodoTask> TodoTasks => Set<TodoTask>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
