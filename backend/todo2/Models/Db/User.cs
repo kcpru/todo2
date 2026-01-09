333fbd5a64eb64a920b4c86a2daa87919e5b482b
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-09 23:28:22 +0100

    Add models and DTOs for user, todo lists, and tasks

---
+++ backend/todo2/Models/Db/User.cs
@@ -0,0 +1,9 @@
+﻿namespace todo2.Models.Db;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+}
