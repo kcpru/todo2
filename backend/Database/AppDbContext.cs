8ff619361a998dde71df8cef2142c661c4851a68
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-09 23:27:18 +0100

    Delete project files from old location

--- backend/Database/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace todo2.Database;
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-}
